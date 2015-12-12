46268261e0e04c25687e4ea587694fbfc22a7f02
Joe O <joseph.opseth@my.uwrf.edu>
2015-12-12 14:12:27 -0600

    Update ExamWindow

--- src/exam/ui/ExamWindow.java
+++ src/exam/ui/ExamWindow.java
@@ -1,11 +1,14 @@
 package exam.ui;
 
+import com.sun.java.util.jar.pack.ConstantPool;
 import exam.Exam;
 import exam.question.Question;
 import exam.question.QuestionPool;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /**
  * A window that takes a question pool file and lets
@@ -14,9 +17,9 @@ import java.awt.*;
 public class ExamWindow extends JFrame {
 
     // Variables for the window
-    private static final int WIDTH = 480;
+    private static final int WIDTH = 640;
     private static final int HEIGHT = 640;
-    private static final int ELEMENT_WIDTH = 300;
+    private static final int ELEMENT_WIDTH = 275;
 
     private JLabel noQuestionsL;
     private JTextField noQuestionsTF;
@@ -33,12 +36,15 @@ public class ExamWindow extends JFrame {
     private JTextArea output;
 
     // Variables for the exam
-    private Exam exam = new Exam();
+    private QuestionPool questionPool;
+    private Exam exam;
     private final String EXAM_FILE = "exam.txt";
     private final String KEY_FILE = "key.txt";
     private int noQuestions, minChapter, maxChapter;
 
-    public ExamWindow(QuestionPool questionPool) {
+    public ExamWindow(QuestionPool questionPoolP) {
+
+        questionPool = questionPoolP;
 
         setTitle("Exam");
         setSize(WIDTH, HEIGHT);
@@ -61,21 +67,60 @@ public class ExamWindow extends JFrame {
         pane.setLayout(null);
 
         // set locations and sizes for the elements
+        //noQuestions
         noQuestionsL.setLocation(30, 15);
         noQuestionsL.setSize(ELEMENT_WIDTH, 30);
         noQuestionsTF.setLocation(45 + ELEMENT_WIDTH, 15);
         noQuestionsTF.setSize(ELEMENT_WIDTH, 30);
+        // minChapter
         minChapterL.setLocation(30, 65);
         minChapterL.setSize(ELEMENT_WIDTH, 30);
         minChapterTF.setLocation(45 + ELEMENT_WIDTH, 65);
         minChapterTF.setSize(ELEMENT_WIDTH, 30);
+        // maxChapter
+        maxChapterL.setLocation(30, 105);
+        maxChapterL.setSize(ELEMENT_WIDTH, 30);
+        maxChapterTF.setLocation(45 + ELEMENT_WIDTH, 105);
+        maxChapterTF.setSize(ELEMENT_WIDTH, 30);
+        // Buttons & Output
+        submitB.setLocation(30, 145);
+        submitB.setSize(ELEMENT_WIDTH, 35);
+        output.setLocation(30, 185);
+        output.setSize(ELEMENT_WIDTH * 2 + 15, 400);
 
+        pane.add(noQuestionsL);
+        pane.add(noQuestionsTF);
+        pane.add(minChapterL);
+        pane.add(minChapterTF);
+        pane.add(maxChapterL);
+        pane.add(maxChapterTF);
+        pane.add(submitB);
+        pane.add(output);
 
+        setVisible(true);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
+    }
+    public static void main(String[] args) {
+        ExamWindow ew = new ExamWindow(new QuestionPool());
+    }
 
-
-
-
+    private class SubmitButtonHandler implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent arg0) {
+            try {
+                noQuestions = Integer.parseInt(noQuestionsTF.getText());
+                minChapter = Integer.parseInt(minChapterTF.getText());
+                maxChapter = Integer.parseInt(maxChapterTF.getText());
+            } catch(NumberFormatException e) {
+                output.setText("Invalid input!");
+            }
+            try {
+                exam = questionPool.makeExam(minChapter, maxChapter, noQuestions);
+            } catch(IndexOutOfBoundsException e) {
+                output.setText("Not enough questions!");
+            }
+        }
     }
 
 
